0871a654259aa4827ee5c4567ace03e79f78ab21
Munipallisri <munipalli.sriram@gmail.com>
2023-06-15 19:23:56 +0530

    added/modified/deleted

--- src/pages/LoginPage.java
+++ src/pages/LoginPage.java
@@ -1,5 +1,12 @@
 package pages;
 
 public class LoginPage {
+	public static void main(String[] args) {
+		System.out.println("Hello Log in Page");
+		String ab="Admin";
+		String bc="Public";
+		System.out.println("login");
+
+	}
 
 }
